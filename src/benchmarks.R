e91ff9318606f277af29b50e9054037396d22826
warbarbye <fryd.wtf@gmail.com>
2020-04-11 17:07:15 +0200

    Clean repo

--- src/benchmarks.R
+++ src/benchmarks.R
@@ -1,14 +1,11 @@
 source("parallel-benchmark.R")
 source("./cma-es/cma-es.R")
-source("./cma-es/cma-es-msr-sigma.R")
-source("./cma-es/cma-es-sigma-quant.R")
-source("./cma-es/cma-es-sigma-JA.R")
 
 
-DIMS = c(2, 10, 30)
-REP = 30
-CPUPC = 0.80
-PROBLEMS = 1:28
+DIMS = c(10, 30)
+REP = 51
+CPUPC = 0.90
+PROBLEMS = 1:30
 
 # CMA-ES-SIGMA-CSA
 
@@ -17,36 +14,36 @@ benchmark_parallel(
                   .probnum = PROBLEMS,
                   .dims = DIMS,
                   .rep = REP,
-		  .cpupc = CPUPC
+        .cpupc = CPUPC
                   )
 
-# CMA-ES-SIGMA-QUANT
+## CMA-ES-SIGMA-QUANT
 
-benchmark_parallel(
-                  .method = cma_es_sigma_quant,
-                  .probnum = PROBLEMS,
-                  .dims = DIMS,
-                  .rep = REP,
-		  .cpupc = CPUPC
-                  )
-# CMA-ES-SIGMA-MSR
+#benchmark_parallel(
+                  #.method = cma_es_sigma_quant,
+                  #.probnum = PROBLEMS,
+                  #.dims = DIMS,
+                  #.rep = REP,
+			#.cpupc = CPUPC
+                  #)
+## CMA-ES-SIGMA-MSR
 
-benchmark_parallel(
-                  .method = cma_es_sigma_msr,
-                  .probnum = PROBLEMS,
-                  .dims = DIMS,
-                  .rep = REP,
-		  .cpupc = CPUPC
-                  )
+#benchmark_parallel(
+                  #.method = cma_es_sigma_msr,
+                  #.probnum = PROBLEMS,
+                  #.dims = DIMS,
+                  #.rep = REP,
+			#.cpupc = CPUPC
+                  #)
 
-# CMA-ES-SIGMA-EXP
+## CMA-ES-SIGMA-EXP
 
-benchmark_parallel(
-                  .method = cma_es_sigma_expth,
-                  .probnum = PROBLEMS,
-                  .dims = DIMS,
-                  .rep = REP,
-		  .cpupc = CPUPC
-                  )
+#benchmark_parallel(
+                  #.method = cma_es_sigma_expth,
+                  #.probnum = PROBLEMS,
+                  #.dims = DIMS,
+                  #.rep = REP,
+			#.cpupc = CPUPC
+                  #)
 
 
