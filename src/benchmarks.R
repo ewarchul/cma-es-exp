eec0fb4c09db6757f82d40f1a59a0809e3177540
Eryk Warchulski <ewarchul@hal.ise.pw.edu.pl>
2020-04-11 03:45:30 +0200

    Set benchmark

--- src/benchmarks.R
+++ src/benchmarks.R
@@ -4,13 +4,49 @@ source("./cma-es/cma-es-msr-sigma.R")
 source("./cma-es/cma-es-sigma-quant.R")
 source("./cma-es/cma-es-sigma-JA.R")
 
+
+DIMS = c(2, 10, 30)
+REP = 30
+CPUPC = 0.80
+PROBLEMS = 1:28
+
+# CMA-ES-SIGMA-CSA
+
+benchmark_parallel(
+                  .method = cma_es,
+                  .probnum = PROBLEMS,
+                  .dims = DIMS,
+                  .rep = REP,
+		  .cpupc = CPUPC
+                  )
+
 # CMA-ES-SIGMA-QUANT
 
 benchmark_parallel(
                   .method = cma_es_sigma_quant,
-                  .probnum = 1:28,
-                  .dims = c(2, 10),
-                  .rep = 30,
-		  .cpupc = .60
+                  .probnum = PROBLEMS,
+                  .dims = DIMS,
+                  .rep = REP,
+		  .cpupc = CPUPC
                   )
+# CMA-ES-SIGMA-MSR
+
+benchmark_parallel(
+                  .method = cma_es_sigma_msr,
+                  .probnum = PROBLEMS,
+                  .dims = DIMS,
+                  .rep = REP,
+		  .cpupc = CPUPC
+                  )
+
+# CMA-ES-SIGMA-EXP
+
+benchmark_parallel(
+                  .method = cma_es_sigma_expth,
+                  .probnum = PROBLEMS,
+                  .dims = DIMS,
+                  .rep = REP,
+		  .cpupc = CPUPC
+                  )
+
 
