ef92ef9b66ab5f37c5457547337fb1bac5c88a1d
Eryk Warchulski <ewarchul@hal.ise.pw.edu.pl>
2020-04-13 12:01:05 +0200

    Delete print from source code of sigma-quant method

--- src/cma-es/cma-es-sigma-quant.R
+++ src/cma-es/cma-es-sigma-quant.R
@@ -116,7 +116,6 @@ cma_es_sigma_quant <- function(par, fn, ..., lower, upper, quant_val=0.09, CMA =
     
     if (log.bestVal) 
       bestVal.log <- rbind(bestVal.log,min(suppressWarnings(min(bestVal.log)), min(arfitness), eval_mean))
-    print(bestVal.log) 
     ## Generate new population:
     arz <- matrix(rnorm(N*lambda), ncol=lambda)
     arx <- xmean + sigma * (BD %*% arz)
