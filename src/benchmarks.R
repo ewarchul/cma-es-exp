5e990e78f87b9137f9ff03e79c910c2204f184ce
warbarbye <fryd.wtf@gmail.com>
2020-04-30 10:19:46 +0200

    Modify code devoted to run benchmarks

--- src/benchmarks.R
+++ src/benchmarks.R
@@ -1,20 +1,17 @@
 source("parallel-benchmark.R")
-source("./cma-es/cma-es.R")
-source("./cma-es/cma-es-sigma-quant.R")
-source("./cma-es/cma-es-sigma-JA-new.R")
-source("./cma-es/cma-es-msr-sigma.R")
-source("./cma-es/cma-es-onefifth-exp-sigma.R")
+source("./des/des-const.R")
+source("./des/des-expth.R")
 DIMS = c(10)
 REP = 51
 CPUPC = 0.9
-PROBLEMS = 16
-tw_send_message("+48697291468", "12056199522", "Computations started!")
+PROBLEMS = 1:30
+METHOD = DES_const
+
 benchmark_parallel(
-                  .method = cma_es_sigma_JA_new,
-                  .probnum = PROBLEMS,
-                  .dims = DIMS,
-                  .rep = REP,
-		  .cpupc = CPUPC
-                  )
-tw_send_message("+48697291468", "12056199522", "cma_es_sigma_JA_new done")
+      .method = METHOD,
+      .probnum = PROBLEMS,
+      .dims = DIMS,
+      .rep = REP,
+      .cpupc = CPUPC
+      )
 
