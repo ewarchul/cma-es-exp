c47d4d8414c6a47e6aad802b6c23d5aaf6a52db5
warbarbye <fryd.wtf@gmail.com>
2020-06-12 00:59:21 +0200

    Develop benchmark function

---
+++ src/benchmark-backend.R
@@ -0,0 +1,86 @@
+library(tidyverse)
+library(cli)
+library(magrittr)
+library(foreach)
+library(doParallel)
+library(cec2017)
+library(cec2013)
+
+#' Benchmark backend
+#'
+#' @description 
+#' Function runs benchmark `.cec` on `.method` for given test functions and dimensionality. 
+#' Evalution on each function is repeated `.rep` times. User is able to
+#' specify usage of CPU cores by `.cpupc` arg.
+#' @param .method optimization algorithm :: function
+#' @param .probnum indices of problem function :: [Int]
+#' @param .dims dimensionalities :: [Int]
+#' @param .rep amount of repetition :: Int
+#' @param .cec year of benchmark :: Int
+#' @param .cpupc CPU usage in pct :: Int
+
+benchmark_parallel = function(.method, .probnum, .dims, .rep, .cec = 17, .cpupc = .75, .write_flag = FALSE) {
+  cli::cli_alert("(problem, dimension, repetition)\n")
+  if (.cec == 17) {
+    scores <- seq(100, 3000, by = 100)
+  } else {
+    scores <- c(seq(-1400, -100, by = 100), seq(100, 1400, 100)) + 1500
+  }
+  no_cores <- floor(.cpupc * detectCores())
+  registerDoParallel(no_cores)
+  for (d in .dims) {
+    results <- foreach(
+      n = .probnum,
+      .combine = c,
+      .export = c("scores", "d")
+    ) %dopar% {
+      resultVector <- c()
+      resets <- c()
+      informMatrix <- matrix(0, nrow = 14, ncol = .rep)
+      for (i in 1:.rep) {
+        result <- tryCatch(
+          {
+            cli::cli_alert_info("Start ({n}, {d}, {i})\n")
+            .method(
+              rep(0, d),
+              fn = function(x) {
+                if (.cec == 17) {
+                  cec2017::cec2017(n, x)
+                } else {
+                  cec2013::cec2013(n, x) + 1500
+                }
+              },
+              lower = -100,
+              upper = 100
+            )
+          },
+          error =
+            function(cond) {
+              print(paste("Dim:", d, " Problem:", n, " ", cond))
+            }
+        )
+        resultVector <- c(resultVector, abs(result$value - scores[n]))
+        resets <- c(resets, result$resets)
+        recordedTimes <- c(0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+        for (bb in 1:length(recordedTimes)) {
+          informMatrix[bb, i] <- abs(result$diagnostic$bestVal[recordedTimes[bb] * ceiling(nrow(result$diagnostic$bestVal)), ] - scores[n])
+        }
+        cli::cli_alert_success("Done ({n}, {d}, {i})\n")
+      }
+      if(.write_flag) {
+        write.table(resultVector, file = paste(paste0("../data/cec", .cec, "/N/N"), n, "D", d, result$label, sep = "-"), sep = ",")
+        write.table(informMatrix, file = paste(paste0("../data/cec", .cec, "/M/"), result$label, "-", n, "-", d, ".txt", sep = ""), sep = ",", col.names = F, row.names = F)
+      }
+      cat(stringr::str_interp(
+"Statistics:
+  Median: ${median(resultVector)}
+  Mean: ${mean(resultVector)}
+  Max: ${max(resultVector)}
+  Min: ${min(resultVector)}
+  Std: ${sd(resultVector)}\n"
+                              )
+          )
+    }
+  }
+  stopImplicitCluster()
+}
--- src/benchmarks.R
+++ src/benchmarks.R
@@ -1,17 +1,55 @@
-source("parallel-benchmark.R")
-source("./des/des-const.R")
-source("./des/des-expth.R")
-DIMS = c(10)
-REP = 51
-CPUPC = 0.9
-PROBLEMS = 1:30
-METHOD = DES_const
+library(here)
+library(yaml)
+source(here::here("src", "benchmark-backend.R"))
+#source(here::here("src", "alg", "cma-es.R"))
+#DIMS = c(10)
+#REP = 51
+#CPUPC = 0.9
+#PROBLEMS = 1:30
+#METHOD = cma_es
 
-benchmark_parallel(
-      .method = METHOD,
-      .probnum = PROBLEMS,
-      .dims = DIMS,
-      .rep = REP,
-      .cpupc = CPUPC
+parse_yaml_config = function(filename) {
+  config = 
+    yaml::read_yaml(filename)
+  config$methods %>%
+    purrr::walk(function(method) {
+      source(here::here("src", "alg", paste0(stringr::str_replace(method, "_", "-"), ".R")))
+    })
+  config$methods_sym = 
+    config$methods %>%
+    purrr::map(base::get)
+  config
+}
+
+parse_config = function(config) {
+  if (is.list(config))
+    config
+  else
+    parse_yaml_config(config)
+}
+
+run_benchmark = function(config) {
+  parsed_config = 
+    parse_config(config)
+  parsed_config$methods_sym %>%
+    purrr::map(function(method) {
+      benchmark_parallel(
+                     .method = method,
+                     .probnum = parsed_config$probnum,
+                     .cec = parsed_config$cec,
+                     .dims = parsed_config$dims,
+                     .rep = parsed_config$repnum,
+                     .cpupc = parsed_config$cpu
+
       )
+    })
+}
+
+#benchmark_parallel(
+      #.method = METHOD,
+      #.probnum = PROBLEMS,
+      #.dims = DIMS,
+      #.rep = REP,
+      #.cpupc = CPUPC
+      #)
 
