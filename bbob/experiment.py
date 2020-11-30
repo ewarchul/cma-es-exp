9f933d1c1ff4929cffe5f6dd77c9270b01b89aba
ewarchul <fryd.wtf@gmail.com>
2020-11-30 13:28:20 +0100

    Add fmin wrappers

--- bbob/experiment.py
+++ bbob/experiment.py
@@ -5,6 +5,26 @@ import scipy.optimize
 from numpy.random import rand
 import os, webbrowser
 
+import cma_es as cma
+
+def fmin_csa(problem, x0):
+    alg = cma.CMA_ES_CSA(
+        x0 = x0,
+        lower = problem.lower_bounds,
+        upper = problem.upper_bounds,
+        full_display = False
+    )
+    return alg.fmin(problem)
+
+def fmin_ppmf(problem, x0):
+    alg = cma.CMA_ES_PPMF(
+        x0 = x0,
+        lower = problem.lower_bounds,
+        upper = problem.upper_bounds,
+        full_display = False
+    )
+    return alg.fmin(problem)
+
 
 def run_experiment(suite_name, output, fmin, budget_multiplier = 1):
     """
@@ -22,13 +42,15 @@ def run_experiment(suite_name, output, fmin, budget_multiplier = 1):
         x0 = problem.initial_solution
         while (problem.evaluations < problem.dimension * budget_multiplier
                and not problem.final_target_hit):
-            fmin(problem, x0, disp=False) 
+            fmin(problem, x0) 
             x0 = problem.lower_bounds + ((rand(problem.dimension) + rand(problem.dimension)) *
                         (problem.upper_bounds - problem.lower_bounds) / 2)
         minimal_print(problem, final=problem.index == len(suite) - 1)
 
 if __name__ == "__main__":
-    run_experiment("bbob", "scipyAlg", scipy.optimize.fmin)
+    run_experiment("bbob", "CMA_ES_PPMF", fmin_ppmf)
+
+
 
 
 
