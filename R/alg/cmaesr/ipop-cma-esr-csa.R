abc88dac59020ab89be7718c3fadebcda4fef32f
ewarchul <ewarchul@gmail.com>
2020-12-15 20:53:05 +0100

    Final version of cmaesr

--- R/alg/cmaesr/ipop-cma-esr-csa.R
+++ R/alg/cmaesr/ipop-cma-esr-csa.R
@@ -1,7 +1,7 @@
 library(BBmisc)
 library(checkmate)
 
-cma_esr_csa = function(
+ipop_cma_esr_csa = function(
   par,
   fn,
   ...,
@@ -18,7 +18,11 @@ cma_esr_csa = function(
   budget = 10000 * n
   stop_ons_list = 
     c(
-      list(stopOnTolX(1e-12)),
+      list(stopOnTolX()),
+      list(stopOnCondCov()),
+      list(stopOnIndefCovMat()),
+      list(stopOnNoEffectAxis()),
+      list(stopOnNoEffectCoord()),
       list(stopOnMaxEvals(budget))
     )
   stop.ons = getCMAESParameter(control, "stop.ons", stop_ons_list)
@@ -26,19 +30,18 @@ cma_esr_csa = function(
     stopf("There must be at least one stopping condition!")
   }
   assertList(stop.ons, min.len = 1L, types = "cma_stopping_condition")
-
   # alwas check for indefinit covariance matrix first
   stop.ons = c(list(stopOnIndefCovMat()), stop.ons)
-
   # restart mechanism (IPOP-CMA-ES)
-  restart.triggers = getCMAESParameter(control, "restart.triggers", character(0L))
+  restart.triggers = list("conditionCov", "noEffectCoord", "noEffectAxis", "tolX", "indefCovMat")
+#  restart.triggers = getCMAESParameter(control, "restart.triggers", character(0L))
   stop.ons.names = sapply(stop.ons, function(stop.on) stop.on$code)
   if (!isSubset(restart.triggers, stop.ons.names)) {
     stopf("Only codes / short names of active stopping conditions allowed as restart trigger, but '%s' are no stopping conditions.", collapse(setdiff(restart.triggers, stop.ons.names), sep = ", "))
   }
   restart.multiplier = getCMAESParameter(control, "restart.multiplier", 2)
   assertNumber(restart.multiplier, lower = 1, na.ok = FALSE, finite = TRUE)
-  max.restarts = getCMAESParameter(control, "max.restarts", 0L)
+  max.restarts = getCMAESParameter(control, "max.restarts", 100)
   assertInt(max.restarts)
 
   #FIXME: default value should be derived from bounds
@@ -249,7 +252,7 @@ cma_esr_csa = function(
       past.time = as.integer(difftime(Sys.time(), start.time, units = "secs")),
       n.iters = iter - 1L,
       n.restarts = run,
-      label = "cma_esr_csa",
+      label = "ipop_cma_esr_csa",
       population.trace = population.trace,
       diagnostic = log,
       message = stop.obj$stop.msgs,
