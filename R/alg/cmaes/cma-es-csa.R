88195e9da3f76b0d297bd584d3466f585467baf6
ewarchul <ewarchul@gmail.com>
2020-12-27 19:46:36 +0100

    Fix termination condition CSA

--- R/alg/cmaes/cma-es-csa.R
+++ R/alg/cmaes/cma-es-csa.R
@@ -27,8 +27,8 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
   ## Parameters:
   trace       <- controlParam("trace", FALSE)
   fnscale     <- controlParam("fnscale", 1)
-  stopfitness <- controlParam("stopfitness", -Inf)
-  budget      <- controlParam("budget", 10000*N )                     ## The maximum number of fitness function calls
+  stopfitness <- controlParam("stopfitness", -10^-20)
+  budget      <- controlParam("budget", 2*10^5)                     ## The maximum number of fitness function calls
   sigma       <- controlParam("sigma", 1)
   sc_tolx     <- controlParam("stop.tolx", 1e-12 * sigma) ## Undocumented stop criterion
   keep.best   <- controlParam("keep.best", TRUE)
@@ -37,13 +37,14 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
   ## Logging options:
   log.all    <- controlParam("diag", TRUE)
   log.sigma  <- controlParam("diag.sigma", log.all)
+  log.budget  <- controlParam("diag.budget", log.all)
   log.eigen  <- controlParam("diag.eigen", log.all)
   log.value  <- controlParam("diag.value", log.all)
   log.pop    <- controlParam("diag.pop", log.all)
   log.bestVal<- controlParam("diag.bestVal", log.all)
   
   ## Strategy parameter setting (defaults as recommended by Nicolas Hansen):
-  lambda      <- controlParam("lambda", 4 + floor(3*log(N)))
+  lambda      <- controlParam("lambda", 4 * N)
   maxiter     <- controlParam("maxit", round(budget/lambda))
   mu          <- controlParam("mu", floor(lambda/2))
   weights     <- controlParam("weights", log(mu+1) - log(1:mu))
@@ -71,6 +72,8 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
   ## Preallocate logging structures:
   if (log.sigma)
     sigma.log <- numeric(maxiter)
+  if (log.budget)
+    budget.log <- numeric(maxiter)
   if (log.eigen)
     eigen.log <- matrix(0, nrow=maxiter, ncol=N)
   if (log.value)
@@ -95,13 +98,13 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
   cviol <- 0L     ## Number of constraint violations
   msg <- NULL     ## Reason for terminating
   nm <- names(par) ## Names of parameters
+
   
   ## Preallocate work arrays:
   arx <- matrix(0.0, nrow=N, ncol=lambda)
   #arx <-  replicate(lambda, runif(N,lower,upper))
   arfitness <- rep(Inf, lambda)
   #arfitness <- apply(arx, 2, function(x) fn(x, ...) * fnscale)
-  counteval <- counteval + lambda
   while (counteval < budget) {
     iter <- iter + 1L
     
@@ -112,27 +115,27 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
     if (log.sigma)
       sigma.log[iter] <- sigma
     
-#    if (log.bestVal) 
-      #bestVal.log <- rbind(bestVal.log,min(suppressWarnings(min(bestVal.log)), min(arfitness)))
-
     ## Generate new population:
     arz <- matrix(rnorm(N*lambda), ncol=lambda)
     arx <- xmean + sigma * (BD %*% arz)
+
     vx <- ifelse(arx > lower, ifelse(arx < upper, arx, upper), lower)
+
     if (!is.null(nm))
       rownames(vx) <- nm
     pen <- 1 + colSums((arx - vx)^2)
     pen[!is.finite(pen)] <- .Machine$double.xmax / 2
     cviol <- cviol + sum(pen > 1)
-    
+
     if (vectorized) {
       y <- fn(vx, ...) * fnscale
     } else {
       y <- apply(vx, 2, function(x) fn(x, ...) * fnscale)
     }
     counteval <- counteval + lambda
-    
+
     arfitness <- y * pen
+
     valid <- pen <= 1
     if (any(valid)) {
       wb <- which.min(y[valid])
@@ -164,9 +167,10 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
     ps <- (1-cs)*ps + sqrt(cs*(2-cs)*mueff) * (B %*% zmean)
     hsig <- drop((norm(ps)/sqrt(1-(1-cs)^(2*counteval/lambda))/chiN) < (1.4 + 2/(N+1)))
     pc <- (1-cc)*pc + hsig * sqrt(cc*(2-cc)*mueff) * drop(BD %*% zmean)
-    
+
     ## Adapt Covariance Matrix:
     BDz <- BD %*% selz
+
     if (if_CMA) 
       C <- (1-ccov) * C + ccov * (1/mucov) *
         (pc %o% pc + (1-hsig) * cc*(2-cc) * C) +
@@ -176,16 +180,20 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
     
     ## Adapt step size sigma:
     sigma <- sigma * exp((norm(ps)/chiN - 1)*cs/damps)
+
     
     e <- eigen(C, symmetric=TRUE)
     eE <- eigen(cov(t(arx)))
     if (log.eigen)
       eigen.log[iter,] <- rev(sort(eE$values))
     
-    if (!all(e$values >= sqrt(.Machine$double.eps) * abs(e$values[1]))) {      
-      msg <- "Covariance matrix 'C' is numerically not positive definite."
-      break
-    }
+   # if (!all(e$values >= sqrt(.Machine$double.eps) * abs(e$values[1]))) {      
+   #   msg <- "Covariance matrix 'C' is numerically not positive definite."
+   #   break
+   # }
+
+    if (log.budget)
+      budget.log[iter] <- counteval
     
     B <- e$vectors
     D <- diag(sqrt(e$values), length(e$values))
@@ -200,10 +208,10 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
     ## Check stop conditions:
     
     ## Condition 1 (sd < tolx in all directions):
-    if (all(D < sc_tolx) && all(sigma * pc < sc_tolx)) {
-      msg <- "All standard deviations smaller than tolerance."
-      break
-    }
+ #   if (all(D < sc_tolx) && all(sigma * pc < sc_tolx)) {
+ #     msg <- "All standard deviations smaller than tolerance."
+ #     break
+ #   }
     
     ## Escape from flat-land:
     if (arfitness[1] == arfitness[min(1+floor(lambda/2), 2+ceiling(lambda/4))]) { 
@@ -211,6 +219,7 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
     if (trace)
       message("Flat fitness function. Increasing sigma.")
     }
+
     if (trace)
       message(sprintf("Iteration %i of %i: current fitness %f",
                       iter, maxiter, arfitness[1] * fnscale))
@@ -222,6 +231,7 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
   ## where actually performed.
   if (log.value) log$value <- value.log[1:iter,]
   if (log.sigma) log$sigma <- sigma.log[1:iter]
+  if (log.budget) log$budget <- budget.log[1:iter]
   if (log.eigen) log$eigen <- eigen.log[1:iter,]
   if (log.pop)   log$pop   <- pop.log[,,1:iter]
   if (log.bestVal) log$bestVal <- bestVal.log
@@ -231,6 +241,7 @@ cma_es_csa <- function(par, fn, ..., lower, upper, if_CMA = TRUE, control=list()
   res <- list(par=best.par,
               value=best.fit / fnscale,
               counts=cnt,
+              iter = iter,
               convergence=ifelse(iter >= maxiter, 1L, 0L),
               message=msg,
               label = "cma-es-csa",
